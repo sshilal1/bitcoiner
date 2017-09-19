264380c70d153099b8bbba3ddca9e21d97306286
Stephen Shilale <sshilal1@binghamton.edu>
2017-09-19 11:09:44 -0400

    slightly change api layout to include gdax

--- coinbase.js
+++ coinbase.js
@@ -2,9 +2,9 @@ const coinbase = require('coinbase');
 const api = require('./api');
 
 const apiURI = 'https://api.gdax.com';
-const coinbaseURI = 'https://api.coinbase.com/v2/users/:' + api.acctId;
+const coinbaseURI = 'https://api.coinbase.com/v2/users/:' + api.coinbase.acctId;
 
-var client   = new coinbase.Client({'apiKey': api.key, 'apiSecret': api.secret});
+var client   = new coinbase.Client({'apiKey': api.coinbase.key, 'apiSecret': api.coinbase.secret});
 
 client.getAccounts({}, function(err, accounts) {
   accounts.forEach(function(acct) {
