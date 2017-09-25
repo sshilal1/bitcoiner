529f66c9b92c89717673e64c016f5455863d1114
Stephen Shilale <sshilal1@binghamton.edu>
2017-09-25 14:56:49 -0400

    added volume limit

--- bittrex.js
+++ bittrex.js
@@ -1,2 +1,29 @@
 const request = require("request-promise");
 
+var markets = [];
+var dailyVolume = 10000000;
+var btcValue = 3904;
+
+request('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+	.then( function(data) {
+		var json = JSON.parse(data);
+		for (var market of json.result) {
+			if (market.MarketName.startsWith("BTC")) {
+				var volum = market.BaseVolume * btcValue;
+			}
+			else {
+				var volum = market.BaseVolume;
+			}
+			if (volum > dailyVolume) {
+				var obj = {
+					name: market.MarketName,
+					volume: volum
+				};
+				markets.push(obj);
+			}
+		}
+	})
+	.then( function() {
+		markets.sort(function(a,b) { return b.volume - a.volume })
+		console.log(markets);
+	})
