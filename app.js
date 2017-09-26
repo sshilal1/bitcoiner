5c806e938620df442eab8ecd5a8357c8c7db82af
Stephen Shilale <sshilal1@binghamton.edu>
2017-09-26 12:56:05 -0400

    added excel4node

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var xl = require('excel4node');
+
 var GdaxClient = require('./gdax-client.js');
 var Bittrex = require('./bittrex.js');
 
@@ -23,6 +25,25 @@ app.get('/ticker', function(req,res) {
 	bittrexClient.getTicker('usdt-btc', function(data) {
 		res.send(data);
 	})
+})
+app.get('/print', function(req,res) {
+	// Create a new instance of a Workbook class 
+	var wb = new xl.Workbook();
+	var ws = wb.addWorksheet('Sheet 1');
+
+	// Create a reusable style 
+	var style = wb.createStyle({
+	    font: {
+	        color: '#FF0800',
+	        size: 12
+	    },
+	    numberFormat: '$#,##0.00; ($#,##0.00); -'
+	});
+	 
+	// Set value of cell A1 to 100 as a number type styled with paramaters of style 
+	ws.cell(1,1).number(100).style(style);
+
+	wb.write('Report.xlsx');
 })
 
 http.listen(3000, function(){
