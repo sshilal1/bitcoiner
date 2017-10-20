19b3404c7da3bda8181e567c593564fe4f9e1466
Stephen Shilale <sshilal1@binghamton.edu>
2017-10-20 17:54:46 -0400

    begin modularity by isolating log creation

---
+++ logging.js
@@ -0,0 +1,23 @@
+const logDir = 'logs';
+const fs = require('fs');
+const winston = require('winston');
+
+class mylog {
+	constructor(bt,st,ct,lt) {
+		var d = new Date()
+		var time = `${d.getHours().toString().padStart(2,0)}.${d.getMinutes().toString().padStart(2,0)}.${d.getSeconds().toString().padStart(2,0)}`;
+		this.filename = ('b'+bt+'s'+st+'c'+ct+'l'+lt+'_'+d.getMonth()+1)+'.'+d.getDate()+'.'+d.getYear()+'_'+time;
+	}
+
+	// create the log file
+	create() {
+
+	}
+
+	// everytime we want to write to the file
+	write(str) {
+
+	}
+}
+
+module.exports = {mylog};
