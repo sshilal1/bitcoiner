29358d82830462d2d4e437511c99bca608ce3340
Stephen Shilale <sshilal1@binghamton.edu>
2017-10-22 22:25:36 -0400

    added consoleLog creation to logging

--- logging.js
+++ logging.js
@@ -2,7 +2,6 @@ const logDir = 'logs';
 const fs = require('fs');
 const winston = require('winston');
 
-// need to further modulate
 class log {
 	constructor(bt,st,ct,lt) {
 		var d = new Date()
@@ -33,18 +32,38 @@ class log {
 	}
 }
 
-module.exports = {log};
+class consoleLog {
+	constructor(bt,st,ct,lt) {
+		var d = new Date()
+		var time = `${d.getHours().toString().padStart(2,0)}.${d.getMinutes().toString().padStart(2,0)}.${d.getSeconds().toString().padStart(2,0)}`;
+		this.filename = ('b'+bt+'s'+st+'c'+ct+'l'+lt+'_'+d.getMonth()+1)+'.'+d.getDate()+'.'+d.getYear()+'_'+time;
+	}
 
-/*
-var logger = new (winston.Logger)({
-  transports: [
-		new (winston.transports.Console)({
-			colorize: true,
-			level: 'info'
-		}),
-    new (winston.transports.File)({
-			filename: `${logDir}/${filename}.log`,
-			timestamp: tsFormat,
+	// create the log file
+	create(name) {
+		var logType = name ? (`_${name}.log`) : '.log';
+		var filename = this.filename;
+		var tsFormat = () => (new Date()).toLocaleString();
+
+		console.log("Creating log file: ", filename);
+
+		this.logger = new (winston.Logger)({
+			transports: [
+				new (winston.transports.Console)({
+					colorize: true,
+					level: 'info'
+				}),
+				new (winston.transports.File)({
+					filename: `${logDir}/${filename}.log`,
+					timestamp: tsFormat,
+				})
+			]
 		})
-	]
-});*/
+	}
+
+	write(message) {
+		this.logger.info(message);
+	}
+}
+
+module.exports = {log, consoleLog};
