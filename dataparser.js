6e76f5594284dd2bae493f881b0faa026968e4ae
Stephen Shilale <sshilal1@binghamton.edu>
2017-10-23 17:11:35 -0400

    working on find data function

--- dataparser.js
+++ dataparser.js
@@ -10,3 +10,21 @@ var loopamounts = ind+10;
 for(i=ind; i<loopamounts; i++) {
 	console.log(arr[i]['ETH-GNO'])
 }
+
+function findData(name, starttime, range, filename) {
+
+	var arr = require(filename);
+
+	var index = _.findIndex(arr, function(o) { return o.time == 50001; });
+
+	var loops = index+range;
+
+	for (let i=index; i<loops; i++) {
+		var iteration = arr[i];
+		var percent = 46;
+
+		var returnStr = `${name} - ${percent}% - (${iteration[name]})`;
+
+		console.log(returnStr);
+	}
+}
