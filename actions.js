ff30d867a3ff12b3698649e2c291e8666d334fdf
Stephen Shilale <sshilal1@binghamton.edu>
2017-10-22 20:12:25 -0400

    added class for actions

---
+++ actions.js
@@ -0,0 +1,18 @@
+
+
+class bittrexActions {
+
+	constructor() {
+		super();
+	}
+
+	buyMarket() {
+		console.log("buying market");
+	}
+
+	sellMarket() {
+		console.log("selling market");
+	}
+}
+
+module.exports = {bittrexActions};
