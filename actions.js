1356d87475cfb6357d1355044ef5e7a58c68acf9
Stephen Shilale <sshilal1@binghamton.edu>
2017-11-01 12:57:48 -0400

    added adjustments to buy/sell

--- actions.js
+++ actions.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 class bittrexActions {
 
 	constructor(logger,reporter) {
@@ -5,24 +7,36 @@ class bittrexActions {
 		this.reporter = reporter;
 	}
 
-	buyMarket(market,timestamp,amount) {
+	buyMarket(market,timestamp,purchases) {
+		market.bought = true;
 		this.logger.write(`Buying ${market.name} at ${market.change}%`);
 		this.reporter.write(`Buying ${market.name} at ${market.change}%`);
-		return {
+		purchases.push({
 			name : market.name,
 			amount : 1,
 			price : market.last,
 			time : timestamp,
 			change : market.change
-		}
+		})
 	}
 
-	sellMarket(market,timestamp,purchase) {
+	sellMarket(market,timestamp,purchases) {
+		market.sold = true;
+		var index = _.findIndex(purchases, function(o) { return o.name == market.name; });
+		var purchase = purchases[index];
+
 		var profit = market.change - purchase.change;
 		this.logger.write(`Selling ${market.name} at ${market.change}%`);
 		this.reporter.write(`Selling ${market.name} at ${market.change}%`);
 		this.logger.write(`Profited ${profit}% from ${market.name}`);
 		this.reporter.write(`Profited ${profit}% from ${market.name}`);
+
+		purchases.splice(index,1);
+	}
+
+	updateSellThreshold(market) {
+		console.log("updating st...");
+		market.st = 20;
 	}
 }
 
