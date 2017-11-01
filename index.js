3d02340f801615f09d4ad2a3fcfc96f6d48cf5bf
Stephen Shilale <sshilal1@binghamton.edu>
2017-11-01 17:10:47 -0400

    changed variable names, fixed typos

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ if (!reRun) {
 				var d = new Date();
 				var timestamp = `${d.getHours().toString().padStart(2,0)}${d.getMinutes().toString().padStart(2,0)}${d.getSeconds().toString().padStart(2,0)}`;
 				
-				var thisobj = {
+				var tick = {
 					time : timestamp
 				}
 
@@ -126,19 +126,18 @@ if (!reRun) {
 								rank++;
 								var pctChange = pdiff(market.Last, market.PrevDay);
 
-								mymarket.change = twenty4HrChange;
+								mymarket.change = pctChange;
 								mymarket.ask = market.Ask;
 								mymarket.low = market.Low;
 
 								var jumper = (mymarket.last < buyThreshold-1) && (mymarket.Last > buyThreshold+1);
 								mymarket.last = market.Last;
 								
-								var floatPct24Change = parseFloat(twenty4HrChange,10);
-
+								var floatPctChange = parseFloat(pctChange,10);
 
 								// If the top 2 coins
 								if (rank < 3) {
-									if ((floatPct24Change > buyThreshold-1) && (floatPct24Change < buyThreshold+1) && !mymarket.bought && !mymarket.neverbuy) {
+									if ((floatPctChange > buyThreshold-1) && (floatPctChange < buyThreshold+1) && !mymarket.bought && !mymarket.neverbuy) {
 										action.buyMarket(mymarket,timestamp,purchases);
 									}
 									else if (jumper) {
@@ -149,7 +148,7 @@ if (!reRun) {
 
 								action.gradientSell(mymarket,timestamp,purchases);
 							}
-							thisobj[market.MarketName] = pdiff(market.Last, market.PrevDay);
+							tick[market.MarketName] = pdiff(market.Last, market.PrevDay);
 						}
 					}
 				}
@@ -175,17 +174,19 @@ if (!reRun) {
 
 				// Write data to history file
 				var arr = require(historyFileName);
-				arr.push(thisobj);
+				arr.push(tick);
 				fs.writeFile(historyFileName, JSON.stringify(arr), (err) => {
 					if (err) throw err;
 				});
 
 			}
+
 			else {
 				logger.write("No Query at " +timestamp);
 			}
+
 		})
-	},1000);
+	}, 5000);
 }
 
 else {
@@ -288,29 +289,6 @@ else {
 // -------------
 // -------------
 // -------------
-// Reporter function for purchases
-// -------------
-function reportOn(newchange,market) {
-	var cross5 = parseInt(buyThreshold,10) + 5;
-	var cross10 = parseInt(buyThreshold,10) - 10;
-	var dip5 = parseInt(buyThreshold,10) - 5;
-	var dip10 = parseInt(buyThreshold,10) - 10;
-	var oldchange = market.change;
-
-	if (oldchange <= cross5 && newchange >= cross5) {
-		reporter.write(`${market.name} Crossing 5% gain from ${oldchange}% to ${newchange}%`);
-	}
-	else if (newchange <= dip5 && oldchange >= dip5) {
-		reporter.write(`${market.name} Dipping 5% loss from ${oldchange}% to ${newchange}%`);
-	}
-	else if (oldchange <= cross10 && newchange >= cross10) {
-		reporter.write(`${market.name} Crossing 10% gain from ${oldchange}% to ${newchange}%`);
-	}
-	else if (newchange <= dip10 && oldchange >= dip10) {
-		reporter.write(`${market.name} Dipping 10% loss from ${oldchange}% to ${newchange}%`);
-	}
-}
-// -------------
 // Percent difference
 // -------------
 function pdiff(first,second) {
@@ -320,21 +298,6 @@ function pdiff(first,second) {
 	return parseFloat(answer,10);
 }
 // -------------
-// Buy function
-// -------------
-function buyMarket(market,msTime,amount) {
-	// Will eventually require padding (check next few seconds to make sure correct buy and not a fluke)
-	logger.write(`Buying ${market.name} at ${market.change}%`);
-	reporter.write(`Buying ${market.name} at ${market.change}%`);
-	purchases.push({
-		name : market.name,
-		amount : 1,
-		price : market.last,
-		time : msTime,
-		change : market.change
-	})
-}
-// -------------
 // Hours Conversion
 // -------------
 var hrs = function(hours) {
@@ -342,105 +305,4 @@ var hrs = function(hours) {
 	return thours > 12 ? thours - 12 : thours;
 }
 // -------------
-// Sell function
 // -------------
-function sellMarket(market, time) {
-	logger.write(`Selling ${market.name} at ${market.change}%`);
-	reporter.write(`Selling ${market.name} at ${market.change}%`);
-	for (let p=0; p<purchases.length; p++) {
-		if (purchases[p].name == market.name) {
-			for (var mymarket of myMarkets) {
-				if (mymarket.name == market.name) {
-					mymarket.sold = true;
-					var profit = pdiff(market.last, purchases[p].price);
-					//var profit = ((market.last - purchases[p].price) * 100 / market.last).toFixed(2);
-					logger.write(`Profited ${profit}% from ${mymarket.name}`);
-					reporter.write(`Profited ${profit}% from ${mymarket.name}`);
-				}
-			}
-			purchases.splice(p,1);
-		}
-	}
-}
-// -------------
-// Print function, writes to excel
-// -------------
-function printData() {
-	var wb = new xl.Workbook();
-	var ws = wb.addWorksheet('Sheet 1');
-
-	var style = wb.createStyle({
-		font: {
-			color: '#FF0800',
-			size: 12
-		}
-	});
-
-	for (var t in timestampHash) {
-		var row = parseInt(t,10) + 1;
-		ws.cell(row,1).string(timestampHash[t]).style(style);
-	}
-
-	var marketCount = 1;
-	for (let i=0; i<10; i++) {
-		var myMarket = myMarkets[i];
-
-		for (var market in marketHistory) {
-			if (market == myMarket.name) {
-				marketCount++;
-				ws.cell(1,marketCount).string(market).style(style);
-
-				for (var tick of marketHistory[market]) {
-					var time = tick.t + 1;
-					ws.cell(time,marketCount).number(tick.v);
-				}
-			}
-		}
-	}
-	wb.write('report.xlsx');
-}
-function printJson() {
-
-	var topMarketsJson = [];
-	var tickCounts = _.size(marketHistory['USDT-BTC']);
-
-	for (let j=1; j<tickCounts; j++) {
-		var query = {
-			time: timestampHash[j]
-		};
-		for (let i=0; i<10; i++) {
-			var name = myMarkets[i].name;
-			var ticker = _.find(marketHistory[name], function(o) {return o.t == j} );
-			//errorlogs.write(`j:${j} ticker:${ticker.toString()}`);
-
-			if (typeof(ticker) != undefined) {
-				query[name] = ticker.v;
-			}
-			else {
-				query[name] = null;
-			}
-		}
-		topMarketsJson.push(query);
-	}
-
-	var historyname = 'b'+buyThreshold;
-
-	jsonfile.writeFileSync(`${historyname}_history.json`, topMarketsJson);
-}
-// -------------
-// Print at node close
-// -------------
-var keypress = require('keypress');
-keypress(process.stdin);
-process.stdin.on('keypress', function (ch, key) {
-	if (key && key.name == 'p') {
-		printData();
-	}
-	if (key && key.name == 'r') {
-		printJson();
-	}
-	if (key && key.ctrl && key.name == 'c') {
-		process.exit();
-	}
-});
-process.stdin.setRawMode(true);
